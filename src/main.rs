5f7296bce0a9df0bc3d341085d277fca9cb04790
Sam Thorold <sam.thorold@gmail.com>
2023-11-05 03:39:12 +0000

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::{
     cmp::{max, min, Ordering},
-    collections::HashMap,
     env,
     fmt::{self, Display, Formatter},
     str::FromStr,
 };
+
+const MAX_SCORE: usize = 10;
+const NODE_MAX_SCORE: i32 = 100;
+const NODE_MIN_SCORE: i32 = -100;
 
 #[derive(Debug)]
 enum PlayerKind {
@@ -61,6 +64,7 @@ struct Cell {
 #[derive(PartialEq, Eq, Hash, Debug, Clone)]
 struct GameState {
     cells: [Cell; 9],
+    to_play: CellValue,
 }
 
 impl Display for GameState {
@@ -93,19 +97,14 @@ impl GameState {
         }
         GameState {
             cells: <[Cell; 9]>::try_from(cells).unwrap(),
+            to_play: CellValue::O,
         }
     }
-    fn next_value(&self) -> CellValue {
-        let mut count: u8 = 9;
-        for cell in &self.cells {
-            if cell.value == CellValue::N {
-                count -= 1;
-            }
-        }
-        if count % 2 == 0 {
-            return CellValue::O;
-        } else {
-            return CellValue::X;
+    fn next_player(&self) -> CellValue {
+        match self.to_play {
+            CellValue::X => CellValue::O,
+            CellValue::O => CellValue::X,
+            CellValue::N => panic!("Should not have N to play."),
         }
     }
     fn next_state(&self, row: usize, col: usize) -> GameState {
@@ -115,7 +114,6 @@ impl GameState {
         if (col < 1) | (col > 9) {
             panic!("Row value invalid.")
         }
-        let value = self.next_value();
         let mut cells = Vec::new();
         for cell in &self.cells {
             let is_changed_cell = (cell.addr.row == row) & (cell.addr.col == col);
@@ -128,13 +126,14 @@ impl GameState {
                     col: cell.addr.col,
                 },
                 value: match is_changed_cell {
-                    true => value,
+                    true => self.to_play,
                     false => cell.value,
                 },
             })
         }
         GameState {
             cells: <[Cell; 9]>::try_from(cells).unwrap(),
+            to_play: self.next_player(),
         }
     }
     fn children(&self) -> Vec<GameState> {
@@ -147,21 +146,118 @@ impl GameState {
         cells
     }
     fn score(&self) -> i32 {
-        todo!()
+        // The depth. "Punish" scores for a high depth.
+        let count_n = self
+            .cells
+            .iter()
+            .filter(|cell| cell.value != CellValue::N)
+            .count();
+        for row in 1..4 {
+            let all_x = self
+                .cells
+                .iter()
+                .filter(|cell| cell.addr.row == row)
+                .all(|cell| cell.value == CellValue::X);
+            if all_x {
+                return -<i32>::try_from(MAX_SCORE - count_n).unwrap();
+            }
+            let all_o = self
+                .cells
+                .iter()
+                .filter(|cell| cell.addr.row == row)
+                .all(|cell| cell.value == CellValue::O);
+            if all_o {
+                return <i32>::try_from(MAX_SCORE - count_n).unwrap();
+            }
+        }
+        for col in 1..4 {
+            let all_x = self
+                .cells
+                .iter()
+                .filter(|cell| cell.addr.col == col)
+                .all(|cell| cell.value == CellValue::X);
+            if all_x {
+                return -<i32>::try_from(MAX_SCORE - count_n).unwrap();
+            }
+            let all_o = self
+                .cells
+                .iter()
+                .filter(|cell| cell.addr.col == col)
+                .all(|cell| cell.value == CellValue::O);
+            if all_o {
+                return <i32>::try_from(MAX_SCORE - count_n).unwrap();
+            }
+        }
+        let all_x = self
+            .cells
+            .iter()
+            .filter(|cell| {
+                (cell.addr.row == 1 && cell.addr.col == 1)
+                    | (cell.addr.row == 2 && cell.addr.col == 2)
+                    | (cell.addr.row == 3 && cell.addr.col == 3)
+            })
+            .all(|cell| cell.value == CellValue::X);
+        if all_x {
+            return -<i32>::try_from(MAX_SCORE - count_n).unwrap();
+        }
+        let all_o = self
+            .cells
+            .iter()
+            .filter(|cell| {
+                (cell.addr.row == 1 && cell.addr.col == 1)
+                    | (cell.addr.row == 2 && cell.addr.col == 2)
+                    | (cell.addr.row == 3 && cell.addr.col == 3)
+            })
+            .all(|cell| cell.value == CellValue::O);
+        if all_o {
+            return <i32>::try_from(MAX_SCORE - count_n).unwrap();
+        }
+        let all_x = self
+            .cells
+            .iter()
+            .filter(|cell| {
+                (cell.addr.row == 3 && cell.addr.col == 1)
+                    | (cell.addr.row == 2 && cell.addr.col == 2)
+                    | (cell.addr.row == 1 && cell.addr.col == 3)
+            })
+            .all(|cell| cell.value == CellValue::X);
+        if all_x {
+            return -<i32>::try_from(MAX_SCORE - count_n).unwrap();
+        }
+        let all_o = self
+            .cells
+            .iter()
+            .filter(|cell| {
+                (cell.addr.row == 3 && cell.addr.col == 1)
+                    | (cell.addr.row == 2 && cell.addr.col == 2)
+                    | (cell.addr.row == 1 && cell.addr.col == 3)
+            })
+            .all(|cell| cell.value == CellValue::O);
+        if all_o {
+            return <i32>::try_from(MAX_SCORE - count_n).unwrap();
+        }
+        0
+    }
+    fn is_terminal(&self) -> bool {
+        let free_cells = self
+            .cells
+            .iter()
+            .filter(|cell| cell.value == CellValue::N)
+            .count();
+        (free_cells == 0) | (self.score() != 0)
     }
 }
 
-#[derive(PartialEq, Eq, Hash, Clone)]
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
 struct Node {
-    state: GameState,
-    depth: i32,
+    states: Vec<GameState>,
     is_maximum: bool,
     is_minimum: bool,
 }
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.state.score().cmp(&other.state.score())
+        self.score().cmp(&other.score())
     }
 }
 
@@ -170,107 +266,110 @@ impl PartialOrd for Node {
         Some(self.cmp(other))
     }
 }
+impl Display for Node {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{:#?} {} {} {} {}\n{}",
+            self.latest_state().to_play,
+            self.is_maximising(),
+            self.score(),
+            self.is_terminal(),
+            self.states.len(),
+            self.latest_state(),
+        )
+    }
+}
 
 impl Node {
+    fn latest_state(&self) -> &GameState {
+        self.states.last().expect("Expect some states.")
+    }
     fn children(&self) -> Vec<Node> {
         let mut nodes = Vec::new();
-        for state in self.state.children() {
+        for state in self.latest_state().children() {
+            let mut states = Vec::new();
+            states.clone_from(&self.states);
+            states.push(state);
             nodes.push(Node {
-                state,
-                depth: self.depth + 1,
+                states,
                 is_maximum: false,
                 is_minimum: false,
             });
         }
         nodes
     }
+    fn to_play(&self) -> CellValue {
+        self.latest_state().to_play
+    }
     fn is_terminal(&self) -> bool {
-        (self.depth == 9) | (self.score() != 0)
+        self.latest_state().is_terminal()
     }
     fn score(&self) -> i32 {
         if self.is_maximum {
-            return 100;
+            return NODE_MAX_SCORE;
         }
         if self.is_minimum {
-            return -100;
+            return NODE_MIN_SCORE;
         }
-        return self.state.score();
+        return self.latest_state().score();
     }
     fn is_maximising(&self) -> bool {
-        return self.state.next_value() == CellValue::O;
+        return self.to_play() == CellValue::O;
     }
     fn minimum(&self) -> Node {
         return Node {
-            state: GameState::new(),
-            depth: 0,
+            states: vec![GameState::new()],
             is_maximum: false,
             is_minimum: true,
         };
     }
     fn maximum(&self) -> Node {
         return Node {
-            state: GameState::new(),
-            depth: 0,
+            states: vec![GameState::new()],
             is_maximum: true,
             is_minimum: false,
         };
     }
 }
 
-/// What is the point of this?
-/// 1. Don't want to calculate states on the fly all the time.
-/// 2. Rust gets _super_ upset about tree-like structures.
-struct Tree {
-    tree: HashMap<Node, Vec<Node>>,
-}
-
-impl Tree {
-    fn new(node: &Node) -> Tree {
-        let mut tree = Tree {
-            tree: HashMap::new(),
-        };
-        tree.tree.insert(node.clone(), node.children());
-        tree
-    }
-    fn children(&mut self, node: &Node) -> &Vec<Node> {
-        if !self.tree.contains_key(node) {
-            self.tree.insert(node.clone(), node.children());
-        }
-        let children = self.tree.get(&node);
-        match children {
-            Some(children) => return children,
-            None => {
-                panic!("Somehow no children.")
-            }
-        }
-    }
-}
-
-fn minimax(node: &Node, a: &Node, b: &Node) -> Node {
+fn minimax(node: &Node, mut a: Node, mut b: Node) -> Node {
+    // println!("Examining\n{}", node);
     if node.is_terminal() {
+        // println!("  Is terminal");
         return node.clone();
     }
-    let best_node = match node.is_maximising() {
+    let mut best_node = match node.is_maximising() {
         true => node.minimum(),
         false => node.maximum(),
     };
     for child in node.children() {
+        let minimax_value = minimax(&child, a.clone(), b.clone());
+        // println!("{:#?} Minimax value\n{}", node.to_play(), minimax_value);
+        // println!("Comparing to\n{}a\n{}b\n{}", best_node, a, b);
         if node.is_maximising() {
-            let minimax_value = minimax(&child, a, b);
-            let best_node = max(&best_node, &minimax_value);
-            let a = max(a, best_node);
-            if best_node >= b {
-                break;
+            best_node = max(best_node.clone(), minimax_value.clone());
+            // println!("Best Node\n{}", best_node);
+            a = max(a.clone(), best_node.clone());
+            // println!("New a\n{}", a);
+            if best_node >= b.clone() {
+                // println!("Greater than b.");
+                // println!("Returning {}", best_node);
+                return best_node.clone();
             }
         } else {
-            let minimax_value = minimax(&child, a, b);
-            let best_node = min(&best_node, &minimax_value);
-            let b = min(b, best_node);
-            if best_node <= a {
-                break;
+            best_node = min(best_node.clone(), minimax_value);
+            // println!("Best Node\n{}", best_node);
+            b = min(b.clone(), best_node.clone());
+            // println!("New b\n{}", b);
+            if best_node <= a.clone() {
+                // println!("Less than a.");
+                // println!("Returning {}", best_node);
+                return best_node.clone();
             }
         };
     }
+    // println!("Returning {}", best_node);
     best_node
 }
 
@@ -279,15 +378,26 @@ fn main() {
     dbg!(&args);
     let game = GameState::new();
     let node = Node {
-        state: game,
-        depth: 0,
+        states: vec![game],
         is_maximum: false,
         is_minimum: false,
     };
+    // println!("{}", node);
+    // for child in node.children() {
+    //     println!("{}", child);
+    //     if child.score() > 0 {
+    //         println!("Latest state\n{}", child.latest_state());
+    //         println!("-- Example states");
+    //         for state in child.states {
+    //             println!("{}", state);
+    //         }
+    //         println!("--");
+    //     }
+    // }
 
-    let mut tree = Tree::new(&node);
-    let children = tree.children(&node);
-    for child in children {
-        println!("{}", child.state)
+    let variation = minimax(&node, node.minimum(), node.maximum());
+    println!("Node {}", variation);
+    for state in variation.states {
+        println!("{}", state);
     }
 }
