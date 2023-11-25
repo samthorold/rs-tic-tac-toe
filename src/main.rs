c74ded1c95e09711cb7814c7a5f620019101a6a5
Sam Thorold <sam.thorold@gmail.com>
2023-11-25 09:17:47 +0000

    Choose player types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use crate::game::GameState;
 use crate::player::{AutoPlayer, InteractivePlayer, Player};
 
@@ -6,12 +8,26 @@ mod player;
 mod search;
 
 fn main() {
+    let mut plyrs = String::new();
+    io::stdin()
+        .read_line(&mut plyrs)
+        .expect("Failed to read line");
+    let mut itr = plyrs.chars();
+    let player1: Box<dyn Player> = match itr.next().unwrap() {
+        'i' => Box::new(InteractivePlayer {}),
+        'a' => Box::new(AutoPlayer {}),
+        _ => panic!("Unknown player type"),
+    };
+    let player2: Box<dyn Player> = match itr.next().unwrap() {
+        'i' => Box::new(InteractivePlayer {}),
+        'a' => Box::new(AutoPlayer {}),
+        _ => panic!("Unknown player type"),
+    };
+
     let mut game = GameState::new();
-    let player1 = InteractivePlayer {};
-    let player2 = AutoPlayer {};
     let mut players: Vec<Box<dyn Player>> = Vec::new();
-    players.push(Box::new(player1));
-    players.push(Box::new(player2));
+    players.push(player1);
+    players.push(player2);
     let mut is_player1 = true;
     while !game.is_terminal() {
         if is_player1 {
